744cb8b05d9fbd5fcf7b38a45a2bfc04221f7ea8
IrynaMulyk <irynamulyk1997@gmail.com>
2023-05-31 01:22:05 +0300

    files path fix

--- src/TransactionsHistory/TransactionHistory.js
+++ src/TransactionsHistory/TransactionHistory.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import css from "/TransactionHistory.module.css";
+import css from "./TransactionHistory.module.css";
 
 const TransactionHistory = ({ transactions }) => {
   return (
