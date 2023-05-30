46c1087ea01f5cf73946edad9dc530f02215338a
IrynaMulyk <irynamulyk1997@gmail.com>
2023-05-31 01:17:34 +0300

    files path fix

--- src/FriendsList/FriendList.js
+++ src/FriendsList/FriendList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
-import FriendListItem from "./FriendListItem";
-import css from "./FriendsList.module.css";
+import FriendListItem from "/FriendListItem";
+import css from "/FriendsList.module.css";
 
 const FriendsList = ({ friends }) => {
   return (
--- src/FriendsList/FriendListItem.js
+++ src/FriendsList/FriendListItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import css from "./FriendsList.module.css";
+import css from "/FriendsList.module.css";
 
 const FriendListItem = ({ friend }) => {
   return (
